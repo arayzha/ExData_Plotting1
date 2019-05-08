13d9a62ff5378f33c661e06007764c2f63a939a9
arayzha <43499528+arayzha@users.noreply.github.com>
2019-05-08 08:14:59 +0700

    Add files via upload

---
+++ Coursera EDA - Project 1.R
@@ -0,0 +1,40 @@
+library(data.table)
+options(digits = 10)
+
+pow = fread("household_power_consumption.txt",sep=';',stringsAsFactors = F,na.strings = '?')
+str(pow)
+pow = subset(pow,pow$Date=='1/2/2007' | pow$Date=='2/2/2007')
+pow$timestamp = as.POSIXct(paste0(pow$Date,' ',pow$Time),format='%d/%m/%Y %H:%M:%S')
+
+
+plot1_data = pow$Global_active_power;
+png('plot1.png',width = 480,height = 480,units = 'px');
+hist(plot1_data,col='red',main='Global Active Power',xlab ='Global Active Power (kilowatt)');
+dev.off();
+
+png('plot2.png',width = 480,height = 480,units = 'px');
+plot(Global_active_power~timestamp,data = pow,type='l',xlab='',ylab='Global Active Power (kilowatt)');
+dev.off();
+
+png('plot3.png',width = 480,height = 480,units = 'px');
+plot(Sub_metering_1~timestamp,data = pow,type='l',xlab='',ylab='Energy sub metering',col='black');
+points(Sub_metering_2~timestamp,data = pow,type='l',col='red');
+points(Sub_metering_3~timestamp,data = pow,type='l',col='blue');
+legend('topright',c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),col=c('black','red','blue'),lwd=1);
+dev.off();
+
+
+png('plot4.png',width = 480,height = 480,units = 'px');
+par(mfrow=c(2,2));
+#4-1
+plot(Global_active_power~timestamp,data = pow,type='l',xlab='',ylab='Global Active Power (kilowatt)');
+#4-2
+plot(Voltage~timestamp,data = pow,type='l',xlab='datetime',ylab='Voltage');
+#4-3
+plot(Sub_metering_1~timestamp,data = pow,type='l',xlab='',ylab='Energy sub metering',col='black');
+points(Sub_metering_2~timestamp,data = pow,type='l',col='red');
+points(Sub_metering_3~timestamp,data = pow,type='l',col='blue');
+legend('topright',c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),col=c('black','red','blue'),lwd=1,bty = 'n');
+#4-4
+plot(Global_reactive_power~timestamp,data = pow,type='l',xlab='datetime',ylab='Global_rective_power');
+dev.off();
